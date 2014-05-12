b401e9304582b74e55fad18231d2d06b48f84703
Weston Platter <westonplatter@gmail.com>
2014-05-11 20:28:30 -0600

    write defensive code

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,10 +1,18 @@
 Spree::Product.class_eval do 
+  
+  # really defensive code because of unknown spree inventory data 
   def brand_name
-    brand.try(:name)
+    brand_id = Spree::Taxonomy.find_by_name('Brand')
+    return "" if brand_id.nil?
+    
+    brand = taxons.find_by_taxonomy_id(brand_id)
+    return "" if brand.nil?
+    
+    brand.name
   end
   
-  def brand
-    brand_id = Spree::Taxonomy.find_by_name('Brand')
-    taxons.find_by_taxonomy_id(brand_id)
-  end
+  # def brand
+  #   brand_id = Spree::Taxonomy.find_by_name('Brand')
+  #   taxons.find_by_taxonomy_id(brand_id)
+  # end
 end
